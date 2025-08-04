b4eae5a1028e900f7c81b03e16122924922e82f5
jerichoRPL <muhammad_husein_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:18:11 +0700

    Add files via upload
    perkenalan

--- jerichoRPL.java
+++ jerichoRPL.java
@@ -1,6 +1,14 @@
 public class jerichoRPL {
 public static void main(String[] args) {
-    System.out.println("jerichooooo");
-}
+    //main method
+    System.out.println("muhammad jericho");
+    System.out.println("SMK Telkom Malang");
+    System.out.println("SMP Negeri 1 Tutur");
+    System.out.println("husein");
+    System.out.println("leny");
+    System.out.println("menjadi miliader di bawah 20 tahun");
+    System.out.println("basketball");
     
+}
 } 
+    
