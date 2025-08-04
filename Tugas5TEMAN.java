32a30882a1982ca5e37b7eb6d7f2edcf492ea594
jerichoRPL <muhammad_husein_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:25:17 +0700

    Add files via upload
    perkenalan

---
+++ Tugas5TEMAN.java
@@ -0,0 +1,78 @@
+public class Tugas5TEMAN {
+
+public static void main(String[] args) {
+    //main method
+    //variable
+    String nama = "jericho";
+    int usia = 15;
+
+    //Tugas5TEMAN
+    //Nama teman
+
+    System.out.println("contoh variable");
+    System.out.println("nama saya adalah" + nama);
+    System.out.println("umur saya adalah:" + usia +"tahun");
+
+   //tugas5teman
+   //nama
+   String namateman1 = "Sakti Mahayana Zaman" ;
+   String namateman2 = "Fatih Lintang Adzahabi";
+    String namateman3 = "Intazar Lintang Pramanta";
+    String namateman4 ="Erlangga Dwi Firmansyah";
+String namateman5 = "Maven Helios Agathon Yesstian";
+
+
+//usia teman
+int usiateman1 = 15;
+int usiateman2 =15;
+int usiateman3 = 15;
+int usiateman4 =15;
+int usiateman5=15;
+
+
+//citacita
+String citacitateman1 = "sukses";
+String citacitateman2 = "manager";
+String citacitateman3 = "it devopler";
+String citacitateman4 = "menjadi orang sukses";
+String citacitateman5 = "web devloper";
+
+
+//output
+//teman
+System.out.println("nama saya adalah " + namateman1);
+System.out.println("usia teman saya adalah " + usiateman1 + "tahun");
+System.out.println("cita cita teman saya adalah " + citacitateman1);
+System.out.println("");
+
+
+//teman2
+System.out.println("nama saya adalah " + namateman2);
+System.out.println("usia teman saya adalah " + usiateman2 + "tahun");
+System.out.println("cita cita teman saya adalah " + citacitateman2);
+System.out.println("");
+
+//teman3
+
+System.out.println("nama saya adalah " + namateman3);
+System.out.println("usia teman saya adalah " + usiateman3 + "tahun");
+System.out.println("cita cita teman saya adalah " + citacitateman3);
+System.out.println("");
+
+
+//teman4 
+System.out.println("nama saya adalah " + namateman4);
+System.out.println("usia teman saya adalah " + usiateman4 + "tahun");
+System.out.println("cita cita teman saya adalah " + citacitateman4);
+System.out.println("");
+
+
+//teman5 
+System.out.println("nama saya adalah " + namateman5);
+System.out.println("usia teman saya adalah " + usiateman5 + "tahun");
+System.out.println("cita cita teman saya adalah " + citacitateman5);
+System.out.println("");
+
+
+}
+}
